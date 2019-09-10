9a0e8bc534a45df4ce26cb7e286bc9bf2dbbb800
JachinLin <45724123+JachinLin-TJ@users.noreply.github.com>
2019-09-11 00:41:20 +0800

    Update Array.hpp

--- train-2/Array.hpp
+++ train-2/Array.hpp
@@ -1,28 +1,29 @@
 #pragma once
 #include <cstddef>
 #include <memory>
+#include <cstring>
 template<typename T>
-class array
+class Array
 {
 public:
     //构造函数
-    array()=default;
-    array(std::size_t n,std::size_t m):rows(n),cols(m)
+    Array()=default;
+    Array(std::size_t n,std::size_t m):rows(n),cols(m)
     {
         data=new T*[n];
         for(int i=0;i<n;i++)
             data[i]=new T[m];
     }
     //析构函数
-    ~array(){clear();}
+    ~Array(){clear();}
     //复制构造函数
-    array(const array& arr){copy_construction(arr);}
+    Array(const Array& arr){copy_construction(arr);}
     //赋值构造函数
-    array& operator = (const array&arr){copy_construction(arr);}
+    Array& operator = (const Array&arr){copy_construction(arr);}
     //移动构造函数
-    array(array&& arr){move_construction(arr);}
+    Array(Array&& arr){move_construction(arr);}
     //移动赋值函数
-    array(array&& arr){move_construction(arr);}
+    Array& operator = (Array&& arr){move_construction(arr);}
 private:
     inline void clear()
     {
@@ -31,7 +32,8 @@ private:
         if(data!=nullptr)
             delete []data;
     }
-    inline void copy_construction(const array& arr)
+    
+    inline void copy_construction(const Array& arr)
     {
         clear();
         data=new T*[rows=arr.rows];
@@ -40,13 +42,21 @@ private:
             data[i]=new T[cols];
         std::memcpy(data,arr.data,sizeof(T)*arr.rows*arr.cols);
     }
-    inline void move_construction(const array&& arr)
+    
+    inline void move_construction(const Array&& arr)
     {
-        std::swap(data,arr.data);
-        std::swap(rows,arr.rows);
-        std::swap(cols,arr.cols);
+        if(this!=&arr)
+        {
+            clear();
+            data=arr.data;
+            arr.data=nullptr;
+            rows=arr.rows;
+            cols=arr.cols;
+        }
+        return *this;
     }
-private:
+    
+public:
     T** data=nullptr;
     std::size_t rows=0;
     std::size_t cols=0;
