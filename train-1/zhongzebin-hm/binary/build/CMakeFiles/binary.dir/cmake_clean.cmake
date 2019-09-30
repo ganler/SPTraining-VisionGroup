dc7dbec11405ddca9c61a58d28861d2dcd0ed42e
zhongzebin <963143327@qq.com>
2019-09-30 18:06:25 +0800

    first commit

---
+++ train-1/zhongzebin-hm/binary/build/CMakeFiles/binary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binary.dir/main.cpp.o"
+  "binary.pdb"
+  "binary"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ train-1/zhongzebin-hm/binary/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "opencv2/opencv.hpp"
+#include <iostream>
+using namespace std;
+using namespace cv;
+int main()
+{
+    VideoCapture cap;
+    cap.open("../project_video.mp4");
+    Mat frame;
+    cap>>frame;
+    while(!frame.empty())
+    {
+cout<<"2"<<endl;
+        Mat gray;
+        Mat binary;
+        cvtColor(frame,gray,COLOR_BGR2GRAY);
+        threshold(gray,binary,int(255/2),255,CV_THRESH_BINARY);
+        imshow("video",binary);
+        waitKey(10);
+        cap>>frame;
+    }
+    cout<<"1"<<endl;
+    return 0;
+}
