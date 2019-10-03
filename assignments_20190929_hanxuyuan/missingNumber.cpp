bb9abd86576b93319ab211773f4753544bc46221
han <365636513@qq.com>
2019-10-03 18:09:21 +0800

    modify missingNumber.cpp

--- assignments_20190929_hanxuyuan/missingNumber.cpp
+++ assignments_20190929_hanxuyuan/missingNumber.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        
+	int n = nums.size();
+	int sum_should_be = n*(n-1)/2;
+	int sum = 0;
+	int missingNumber = 0;
+	for(int i=0; i<n; i++){
+		sum += i;
+	{}
+	missingNumber = n-sum+sum_should_be
+	return missingNumber;
     }
 };
