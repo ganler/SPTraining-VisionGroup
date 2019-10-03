40b643b0e0f171f55712e987416b46a4208c5cc9
han <365636513@qq.com>
2019-10-03 22:43:47 +0800

    add main function to missingNumber.cpp

--- assignments_20190929_hanxuyuan/missingNumber.cpp
+++ assignments_20190929_hanxuyuan/missingNumber.cpp
@@ -1,14 +1,19 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
+#include <iostream>
+#include <vector>
+using namespace std;
+int missingNumber(vector<int>& nums);
+int missingNumber(vector<int>& nums) {
 	int n = nums.size();
 	int sum_should_be = n*(n-1)/2;
 	int sum = 0;
 	int missingNumber = 0;
 	for(int i=0; i<n; i++){
 		sum += i;
-	{}
-	missingNumber = n-sum+sum_should_be
+	}
+	missingNumber = n-sum+sum_should_be;
 	return missingNumber;
     }
 };
+int main(){
+	vector<int>
+}
--- assignments_20190929_hanxuyuan/rotate.cpp
+++ assignments_20190929_hanxuyuan/rotate.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 #include <algorithm>
  
 using namespace std;
