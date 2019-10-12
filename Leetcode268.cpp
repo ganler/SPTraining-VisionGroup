9e7d64bf649909eb6fa2dee5cd968b94cf564969
1104879447 <1104879447@qq.com>
2019-10-12 19:02:18 +0800

    1

--- Leetcode268.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int i=nums.size()*(nums.size()+1)/2-accumulate(nums.begin(),nums.end(),0);
-        return i;
-    }
-};
